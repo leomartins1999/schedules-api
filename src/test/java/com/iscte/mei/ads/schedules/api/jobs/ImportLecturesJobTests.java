cfaad0b5835020b927a1258a7c8e5c09c132a0f5
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2021-10-29 16:04:52 +0100

    Adjust tests for feature

---
+++ src/test/java/com/iscte/mei/ads/schedules/api/jobs/ImportLecturesJobTests.java
@@ -0,0 +1,93 @@
+package com.iscte.mei.ads.schedules.api.jobs;
+
+import com.iscte.mei.ads.schedules.api.entities.Lecture;
+import com.iscte.mei.ads.schedules.api.repositories.LecturesRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class ImportLecturesJobTests {
+
+    private final static long SCHEDULE_ID = 1L;
+
+    private final ImportLecturesJob job;
+
+    private final LecturesRepository repository;
+
+    @Autowired
+    public ImportLecturesJobTests(ImportLecturesJob job, LecturesRepository repository) {
+        this.job = job;
+        this.repository = repository;
+    }
+
+    @BeforeEach
+    void setUp() {
+        repository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Creates no lectures")
+    void createNoLectures() {
+        List<Lecture> lectures = buildLectureList(0);
+
+        job.execute(SCHEDULE_ID, lectures);
+
+        assertEquals(lectures.size(), repository.count());
+    }
+
+    @Test
+    @DisplayName("Creates a lecture")
+    void createLecture() {
+        List<Lecture> lectures = buildLectureList(1);
+
+        job.execute(SCHEDULE_ID, lectures);
+
+        assertEquals(lectures.size(), repository.count());
+    }
+
+    @Test
+    @DisplayName("Creates Multiple Lectures")
+    void createMultipleLectures() {
+        List<Lecture> lectures = buildLectureList(5);
+
+        job.execute(SCHEDULE_ID, lectures);
+
+        assertEquals(lectures.size(), repository.count());
+    }
+
+    private List<Lecture> buildLectureList(int size) {
+        ArrayList<Lecture> result = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) result.add(buildLecture());
+
+        return result;
+    }
+
+    private Lecture buildLecture() {
+        return new Lecture(
+                "Contabilidade Financeira I",
+                "LG",
+                "GAi",
+                "L0638-2T01",
+                "AA2.26",
+                "2015-10-06",
+                "11:00:00",
+                "12:30:00",
+                30,
+                38,
+                "Sala de Aulas normal",
+                "Sala Aulas Mestrado Plus, Hor�rio sala vis�vel portal p�blico, Sala Aulas Mestrado, Sala de Aulas normal",
+                false,
+                false
+        );
+    }
+
+}
--- src/test/java/com/iscte/mei/ads/schedules/api/services/CreateScheduleTests.java
+++ src/test/java/com/iscte/mei/ads/schedules/api/services/CreateScheduleTests.java
@@ -1,18 +1,15 @@
 package com.iscte.mei.ads.schedules.api.services;
 
-import com.iscte.mei.ads.schedules.api.entities.Lecture;
 import com.iscte.mei.ads.schedules.api.entities.Schedule;
 import com.iscte.mei.ads.schedules.api.repositories.LecturesRepository;
 import com.iscte.mei.ads.schedules.api.repositories.SchedulesRepository;
 import com.iscte.mei.ads.schedules.api.utils.IterableUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -39,109 +36,34 @@ public class CreateScheduleTests {
         lecturesRepository.deleteAll();
     }
 
-    @Nested
-    @DisplayName("Schedule Tests")
-    class ScheduleTests {
-        @Test
-        @DisplayName("Create a Schedule")
-        public void createScheduleTest() {
-            Schedule schedule = new Schedule("schedule-name");
+    @Test
+    @DisplayName("Create a Schedule")
+    public void createScheduleTest() {
+        Schedule schedule = new Schedule("schedule-name");
 
-            Schedule result = service.createSchedule(schedule, Collections.emptyList());
+        Schedule result = service.createSchedule(schedule, Collections.emptyList());
 
-            assertEquals("schedule-name", result.getName());
+        assertEquals("schedule-name", result.getName());
 
-            List<Schedule> schedules = IterableUtils.iterableToList(schedulesRepository.findAll());
-            assertEquals(1, schedules.size());
-            assertEquals("schedule-name", schedules.get(0).getName());
-        }
-
-        @Test
-        @DisplayName("Create schedules with equal names")
-        public void createMultipleSchedulesTest() {
-            Schedule firstSchedule = new Schedule("schedule-name");
-            Schedule secondSchedule = new Schedule("schedule-name");
-
-            Schedule first = service.createSchedule(firstSchedule, Collections.emptyList());
-            Schedule second = service.createSchedule(secondSchedule, Collections.emptyList());
-
-            assertEquals("schedule-name", first.getName());
-            assertEquals("schedule-name", second.getName());
-            assert (!first.equals(second));
-
-            List<Schedule> schedules = IterableUtils.iterableToList(schedulesRepository.findAll());
-            assertEquals(2, schedules.size());
-        }
+        List<Schedule> schedules = IterableUtils.iterableToList(schedulesRepository.findAll());
+        assertEquals(1, schedules.size());
+        assertEquals("schedule-name", schedules.get(0).getName());
     }
 
-    @Nested
-    @DisplayName("Lecture Tests")
-    class LectureTests {
+    @Test
+    @DisplayName("Create schedules with equal names")
+    public void createMultipleSchedulesTest() {
+        Schedule firstSchedule = new Schedule("schedule-name");
+        Schedule secondSchedule = new Schedule("schedule-name");
 
-        @Test
-        @DisplayName("Creates no lectures")
-        void createNoLectures() {
-            List<Lecture> lectures = buildLectureList(0);
+        Schedule first = service.createSchedule(firstSchedule, Collections.emptyList());
+        Schedule second = service.createSchedule(secondSchedule, Collections.emptyList());
 
-            service.createSchedule(
-                    new Schedule("schedule-name"),
-                    lectures
-            );
+        assertEquals("schedule-name", first.getName());
+        assertEquals("schedule-name", second.getName());
+        assert (!first.equals(second));
 
-            assertEquals(lectures.size(), lecturesRepository.count());
-        }
-
-        @Test
-        @DisplayName("Creates a lecture")
-        void createLecture() {
-            List<Lecture> lectures = buildLectureList(1);
-
-            service.createSchedule(
-                    new Schedule("schedule-name"),
-                    lectures
-            );
-
-            assertEquals(lectures.size(), lecturesRepository.count());
-        }
-
-        @Test
-        @DisplayName("Creates Multiple Lectures")
-        void createMultipleLectures() {
-            List<Lecture> lectures = buildLectureList(5);
-
-            service.createSchedule(
-                    new Schedule("schedule-name"),
-                    lectures
-            );
-
-            assertEquals(lectures.size(), lecturesRepository.count());
-        }
-
-        private List<Lecture> buildLectureList(int size) {
-            ArrayList<Lecture> result = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) result.add(buildLecture());
-
-            return result;
-        }
-
-        private Lecture buildLecture() {
-            return new Lecture(
-                    "Contabilidade Financeira I",
-                    "LG",
-                    "GAi",
-                    "L0638-2T01",
-                    "AA2.26",
-                    "2015-10-06",
-                    "11:00:00",
-                    "12:30:00",
-                    30,
-                    38,
-                    "Sala de Aulas normal",
-                    "Sala Aulas Mestrado Plus, Hor�rio sala vis�vel portal p�blico, Sala Aulas Mestrado, Sala de Aulas normal",
-                    false,
-                    false
-            );
-        }
+        List<Schedule> schedules = IterableUtils.iterableToList(schedulesRepository.findAll());
+        assertEquals(2, schedules.size());
     }
 }
