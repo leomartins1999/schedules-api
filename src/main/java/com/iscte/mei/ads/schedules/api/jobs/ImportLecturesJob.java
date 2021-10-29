839eab7b68d886659c41be9079465739c543406d
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2021-10-29 16:04:36 +0100

    Add ImportLecturesJob

---
+++ src/main/java/com/iscte/mei/ads/schedules/api/jobs/ImportLecturesJob.java
@@ -0,0 +1,24 @@
+package com.iscte.mei.ads.schedules.api.jobs;
+
+import com.iscte.mei.ads.schedules.api.entities.Lecture;
+import com.iscte.mei.ads.schedules.api.repositories.LecturesRepository;
+import com.iscte.mei.ads.schedules.api.utils.IterableUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ImportLecturesJob {
+
+    private final LecturesRepository repository;
+
+    public ImportLecturesJob(LecturesRepository repository) {
+        this.repository = repository;
+    }
+
+    public void execute(long scheduleId, List<Lecture> lectures) {
+        Iterable<Lecture> entities = IterableUtils.map(lectures, (lecture -> lecture.withScheduleId(scheduleId)));
+
+        repository.saveAll(entities);
+    }
+}
