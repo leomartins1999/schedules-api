157ef07f8b6e6aa0c5e234dca182adb8067813b6
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2021-10-29 01:25:25 +0100

    Add tests for feature

--- src/test/java/com/iscte/mei/ads/schedules/api/controllers/CreateScheduleTests.java
+++ src/test/java/com/iscte/mei/ads/schedules/api/controllers/CreateScheduleTests.java
@@ -2,7 +2,9 @@ package com.iscte.mei.ads.schedules.api.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iscte.mei.ads.schedules.api.deserializers.LectureListDeserializer;
 import com.iscte.mei.ads.schedules.api.entities.Schedule;
+import com.iscte.mei.ads.schedules.api.models.ScheduleFormat;
 import com.iscte.mei.ads.schedules.api.models.WriteSchedule;
 import com.iscte.mei.ads.schedules.api.services.SchedulesService;
 import org.junit.jupiter.api.DisplayName;
@@ -26,20 +28,23 @@ public class CreateScheduleTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @MockBean
     private SchedulesService service;
 
-    @Autowired
-    private ObjectMapper mapper;
+    @MockBean
+    private LectureListDeserializer deserializer;
 
     @Test
     @DisplayName("Creating valid schedule yields created")
     public void createScheduleTest() throws Exception {
         Schedule schedule = new Schedule("my-schedule");
 
-        when(service.createSchedule(any())).thenReturn(schedule);
+        when(service.createSchedule(any(), any())).thenReturn(schedule);
 
-        WriteSchedule model = new WriteSchedule(schedule.getName());
+        WriteSchedule model = new WriteSchedule(schedule.getName(), ScheduleFormat.CSV, "<schedule-content>");
 
         mockMvc.perform(buildCreateScheduleRequest(model))
                 .andExpect(status().isCreated())
@@ -56,7 +61,7 @@ public class CreateScheduleTests {
     @Test
     @DisplayName("Create schedule with null name yields bad request")
     public void createScheduleWithNullNameTest() throws Exception {
-        WriteSchedule model = new WriteSchedule(null);
+        WriteSchedule model = new WriteSchedule(null, ScheduleFormat.CSV, "<schedule-content>");
 
         mockMvc.perform(buildCreateScheduleRequest(model))
                 .andExpect(status().isBadRequest());
@@ -65,7 +70,7 @@ public class CreateScheduleTests {
     @Test
     @DisplayName("Create schedule with blank name yields bad request")
     public void createScheduleWithBlankNameTest() throws Exception {
-        WriteSchedule model = new WriteSchedule("");
+        WriteSchedule model = new WriteSchedule("", ScheduleFormat.CSV, "<schedule-content>");
 
         mockMvc.perform(buildCreateScheduleRequest(model))
                 .andExpect(status().isBadRequest());
@@ -74,7 +79,54 @@ public class CreateScheduleTests {
     @Test
     @DisplayName("Create schedule with empty name yields bad request")
     public void createScheduleWithEmptyNameTest() throws Exception {
-        WriteSchedule model = new WriteSchedule(" ");
+        WriteSchedule model = new WriteSchedule(" ", ScheduleFormat.CSV, "<schedule-content>");
+
+        mockMvc.perform(buildCreateScheduleRequest(model))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Create schedule with null format yields bad request")
+    public void createScheduleWithNullFormatTest() throws Exception {
+        WriteSchedule model = new WriteSchedule("schedule-name", null, "<schedule-content>");
+
+        mockMvc.perform(buildCreateScheduleRequest(model))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Create schedule with null content yields bad request")
+    public void createScheduleWithNullContentTest() throws Exception {
+        WriteSchedule model = new WriteSchedule("Schedule-name", ScheduleFormat.CSV, null);
+
+        mockMvc.perform(buildCreateScheduleRequest(model))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Create schedule with blank content yields bad request")
+    public void createScheduleWithBlankContentTest() throws Exception {
+        WriteSchedule model = new WriteSchedule("Schedule-name", ScheduleFormat.CSV, "");
+
+        mockMvc.perform(buildCreateScheduleRequest(model))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Create schedule with empty content yields bad request")
+    public void createScheduleWithEmptyContentTest() throws Exception {
+        WriteSchedule model = new WriteSchedule("Schedule-name", ScheduleFormat.CSV, " ");
+
+        mockMvc.perform(buildCreateScheduleRequest(model))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Create schedule with invalid content yields bad request")
+    public void createScheduleWithInvalidContentTest() throws Exception {
+        WriteSchedule model = new WriteSchedule("Schedule-name", ScheduleFormat.CSV, "some content");
+
+        when(deserializer.deserialize(any(), any())).thenThrow(new IllegalArgumentException());
 
         mockMvc.perform(buildCreateScheduleRequest(model))
                 .andExpect(status().isBadRequest());
