5705e55937dd44ef0de6a208dddc327a33ff885f
MachadoS21 <samuelbcm99@gmail.com>
2021-11-02 16:37:27 +0000

    Revert "Get classes for schedule endpoint"
    This reverts commit 085acea1e518be8134a29209a7567a8e07da4cd5.

--- src/main/java/com/iscte/mei/ads/schedules/api/repositories/LecturesRepository.java
+++ src/main/java/com/iscte/mei/ads/schedules/api/repositories/LecturesRepository.java
@@ -4,8 +4,4 @@ import com.iscte.mei.ads.schedules.api.entities.Lecture;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LecturesRepository extends CrudRepository<Lecture, Long> {
-
-	@Query("SELECT klass FROM lecture l WHERE lecture.scheduleId = schedule.id")
-	public List<klass> getByScheduleId(Schedule schedule)
-
 }
