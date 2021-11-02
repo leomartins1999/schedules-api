1e30fbfeed74f7b64512274e730b8de6af9db487
MachadoS21 <samuelbcm99@gmail.com>
2021-11-02 18:33:25 +0000

    Get lectures and dates for classes endpoint

--- src/main/java/com/iscte/mei/ads/schedules/api/repositories/LecturesRepository.java
+++ src/main/java/com/iscte/mei/ads/schedules/api/repositories/LecturesRepository.java
@@ -6,6 +6,13 @@ import org.springframework.data.repository.CrudRepository;
 public interface LecturesRepository extends CrudRepository<Lecture, Long> {
 
 	@Query("SELECT klass FROM lecture l WHERE lecture.scheduleId = schedule.id")
-	public List<klass> getByScheduleId(Schedule schedule)
+	public List<klass> getByScheduleId(Schedule schedule);
+
+	@Query("SELECT lecture FROM lecture l WHERE l.klass = klass")
+	public List<Lecture> findAll(Klass klass);
+
+	@Query("SELECT day FROM lecture l WHERE l.klass = klass")
+	public List<day> findAll(Klass klass);
+
 
 }
