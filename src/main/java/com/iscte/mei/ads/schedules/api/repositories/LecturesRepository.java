855613174c0991b68ba018f6f45c22f5200812d9
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2021-11-14 18:34:50 +0000

    fixup! Implement CalculateScoresJob

--- src/main/java/com/iscte/mei/ads/schedules/api/repositories/LecturesRepository.java
+++ src/main/java/com/iscte/mei/ads/schedules/api/repositories/LecturesRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface LecturesRepository extends CrudRepository<Lecture, Long> {
 
-    @Query("SELECT " +
-            "CAST(COUNT(*) FILTER ( WHERE signed_up_for_class > max_number_of_students_for_room ) AS DECIMAL) " +
-            "/ GREATEST(COUNT(*), 1) " +
-            "FROM lecture " +
-            "WHERE schedule_id = :scheduleId;")
+    @Query("SELECT "
+            + "CAST(COUNT(*) FILTER ( WHERE signed_up_for_class > max_number_of_students_for_room ) AS DECIMAL) "
+            + "/ GREATEST(COUNT(*), 1) "
+            + "FROM lecture "
+            + "WHERE schedule_id = :scheduleId;")
     float getPctOverflowingLectures(long scheduleId);
 
 }
