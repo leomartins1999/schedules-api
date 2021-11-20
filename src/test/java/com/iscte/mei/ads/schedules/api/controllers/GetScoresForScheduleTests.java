2f2e7bbed84cedd6497d6a313cacca912b5afb7f
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2021-11-20 19:49:14 +0000

    Add tests for feature

---
+++ src/test/java/com/iscte/mei/ads/schedules/api/controllers/GetScoresForScheduleTests.java
@@ -0,0 +1,65 @@
+package com.iscte.mei.ads.schedules.api.controllers;
+
+import com.iscte.mei.ads.schedules.api.deserializers.LectureListDeserializer;
+import com.iscte.mei.ads.schedules.api.entities.Score;
+import com.iscte.mei.ads.schedules.api.services.SchedulesService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.NoSuchElementException;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SchedulesController.class)
+public class GetScoresForScheduleTests {
+
+    private static final String ENDPOINT_URL = "/schedules/1/scores";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SchedulesService service;
+
+    @MockBean
+    private LectureListDeserializer deserializer;
+
+    @Test
+    @DisplayName("Gets the scores for a schedule")
+    void getScore() throws Exception {
+        Score score = new Score(1, 0.5F);
+
+        when(service.getScoresForSchedule(score.getScheduleId())).thenReturn(score);
+
+        mockMvc.perform(get(ENDPOINT_URL))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.schedule_id").value(score.getScheduleId()))
+                .andExpect(jsonPath("$.pct_overflowing_lectures").value(0.5));
+    }
+
+    @Test
+    @DisplayName("If the schedule does not exist, responds with 404")
+    void scheduleNotFound() throws Exception {
+        when(service.getScoresForSchedule(1)).thenThrow(NoSuchElementException.class);
+
+        mockMvc.perform(get(ENDPOINT_URL))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("If the schedule has not finished calculating, responds with 409")
+    void scheduleNotFinishedCalculating() throws Exception {
+        when(service.getScoresForSchedule(1)).thenThrow(IllegalStateException.class);
+
+        mockMvc.perform(get(ENDPOINT_URL))
+                .andExpect(status().isConflict());
+    }
+
+}
